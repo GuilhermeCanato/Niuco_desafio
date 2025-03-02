06456192655ca80c82ab68a68c18cf2e797a34a8
Guilherme Canato <gui.mcanato2@gmail.com>
2025-03-02 19:00:30 -0300

    feat: inicio de desenvolvimento da API

--- main.py
+++ main.py
@@ -2,13 +2,52 @@ from source.land                import Land
 from source.probe               import Probe
 from source.probe_controller    import ProbeController
 
-land = Land(5, 5)
-probe_controller = ProbeController(land)
+from fastapi    import FastAPI, HTTPException
+from pydantic   import BaseModel
+from typing     import List
 
-probe_controller.add_probe(1, 2, "N", "LMLMLMLMM")
-probe_controller.add_probe(3, 3, "E", "MMRMMRMRRM")
+app = FastAPI()
 
-# probe_controller.show_probes_position()
-# probe_controller.execute_probes()
-# probe_controller.show_probes_position()
-print(probe_controller.show_probes_values())
+class LandEntry(BaseModel):
+    height: int
+    width: int
+
+class ProbeEntry(BaseModel):
+    x: int
+    y: int
+    direction: str
+    command: str
+
+class EntryData(BaseModel):
+    land: LandEntry
+    probes: List[ProbeEntry]
+
+@app.post("/entry-data")
+def data_entry(data: EntryData):
+    try:
+        land_entry = data.land
+        probes_entry = data.probes
+
+        land = Land(land_entry.height, land_entry.width)
+        probe_controller = ProbeController(land)
+
+        for probe in probes_entry:
+            probe_controller.add_probe(probe.x, probe.y, probe.direction, probe.command)
+
+        probe_controller.execute_probes()
+
+        return {
+            "sondas": [probe.model_dump() in probe_controller.show_probes_values()]
+            
+        }
+    except Exception as e:
+        raise HTTPException(status_code = 400, detail = f"Error: {e}")
+
+
+# 
+# probe_controller.add_probe(3, 3, "E", "MMRMMRMRRM")
+
+# # probe_controller.show_probes_position()
+# # probe_controller.execute_probes()
+# # probe_controller.show_probes_position()
+# print(probe_controller.show_probes_values())
